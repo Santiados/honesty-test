d3134a4e509032389104353d6e967ed129c47e3c
Santi <santireven20@gmail.com>
2018-11-29 18:03:43 +0100

    hora_ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -42,7 +42,7 @@ export class HomePage {
       this._SESSIONS = [];
       for(let d in ses){
         if(ses[d].id_user1 == this.user.getId() || ses[d].id_user2 == this.user.getId()){
-          let fecha = (new Date(ses[d].last_msg_time).getHours()) + ':'+ (new Date(ses[d].last_msg_time).getMinutes());
+          let fecha = (new Date(ses[d].last_msg_time).getHours()) + ':'+ ((new Date(ses[d].last_msg_time).getMinutes() < 10) ? '0'+new Date(ses[d].last_msg_time).getMinutes(): new Date(ses[d].last_msg_time).getMinutes() );
           let session = new Session(ses[d].id,ses[d].id_user1,ses[d].username_user1,ses[d].id_user2,ses[d].username_user2,ses[d].last_msg,fecha,ses[d].sessionOpen);
           this._SESSIONS.push(session);
         } 
